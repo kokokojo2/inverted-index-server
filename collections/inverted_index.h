645b288783dc428fc0461587725ad84a6db33a6d
Dmytro <dmytro.nasikan@oak-tree.us>
2022-12-17 20:41:35 +0200

    logic of adding to index was changed due to changes in Set synchronization mechanisms

--- collections/inverted_index.h
+++ collections/inverted_index.h
@@ -10,12 +10,9 @@ class ConcurrentInvertedIndex : ConcurrentHashTable<Set*> {
 public:
     ConcurrentInvertedIndex() : ConcurrentHashTable<Set*>(6) {};
 
-    // todo: synchronize concurrent set update
     void addToIndex(const std::string& word, std::string docId) {
-        auto set = ConcurrentHashTable<Set*>::get(word, new Set());
+        auto set = ConcurrentHashTable<Set*>::getOrSetDefault(word, new Set());
         set->add(std::move(docId));
-        ConcurrentHashTable<Set*>::set(word, set);
-
     }
 
     Set* getDocIds(const std::string& word) {
