6c958f53b13055533cf2ae2a121bcdda2d5336a1
Dmytro <dmytro.nasikan@oak-tree.us>
2022-12-17 20:39:31 +0200

    Set now inherits from ConcurrentHashTable to provide thread safe interface

--- collections/set.h
+++ collections/set.h
@@ -7,23 +7,22 @@
 #ifndef INVERTED_INDEX_SET_H
 #define INVERTED_INDEX_SET_H
 // does not store any value
-// TODO: factor out implementations to set.cpp file
-class Set : HashTable<bool> {
+class Set : ConcurrentHashTable<bool> {
 public:
-    Set() : HashTable<bool>() {}
+    Set() : ConcurrentHashTable<bool>(512) {}
 
     void add(std::string key) {
-        HashTable<bool>::set(std::move(key), true);
+        ConcurrentHashTable<bool>::set(std::move(key), true);
     }
 
     bool has(std::string key) {
-        return HashTable<bool>::get(std::move(key), false);
+        return ConcurrentHashTable<bool>::get(std::move(key), false);
     }
 
     std::vector<std::string> getItemsVector() {
         std::vector<std::string> items;
-        for (int i = 0; i < HashTable<bool>::size; i++) {
-            auto hashTableNodesVector = HashTable<bool>::array[i]->toVector();
+        for (int i = 0; i < ConcurrentHashTable<bool>::size; i++) {
+            auto hashTableNodesVector = ConcurrentHashTable<bool>::array[i]->toVector();
             for (auto & node : hashTableNodesVector)
                 items.push_back(node->key);
         }
