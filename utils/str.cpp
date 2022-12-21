2d38c997ce6f4e9c4473bcf741f244591136ce69
Dmytro <dmytro.nasikan@oak-tree.us>
2022-12-22 00:25:46 +0200

    vectorToString and joinStrings utility functions were added

--- utils/str.cpp
+++ utils/str.cpp
@@ -19,3 +19,21 @@ std::string cleanString(std::string str) {
     str.erase(remove_if(str.begin(), str.end(), isNotAlNum), str.end());
     return str;
 }
+
+
+std::string vectorToString(const std::vector<std::string>& vector, bool inLine) {
+    std::string result = "[";
+    if (!inLine) result += "\n";
+    for (auto & i : vector)
+        result += i + (inLine ? "," : ",\n");
+    result +=  "]";
+    return result;
+}
+
+std::string joinStrings(const std::vector<std::string>& vector, char delimiter) {
+    std::string result;
+    for (auto & i : vector)
+        result += i + delimiter;
+    return result;
+}
+
--- utils/str.h
+++ utils/str.h
@@ -7,5 +7,7 @@
 #ifndef INVERTED_INDEX_STR_H
 #define INVERTED_INDEX_STR_H
 std::vector<std::string> splitString(const std::string& string, char delimiter);
+std::string vectorToString(const std::vector<std::string>& vector, bool inLine);
 std::string cleanString(std::string str);
+std::string joinStrings(const std::vector<std::string>& vector, char delimiter);
 #endif
