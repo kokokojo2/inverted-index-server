827c362b8f3aaf83d08091e5398426344a35a0a2
Dmytro <dmytro.nasikan@oak-tree.us>
2022-12-22 00:26:03 +0200

    print_vector utility function was added

--- utils/io/output.cpp
+++ utils/io/output.cpp
@@ -6,3 +6,11 @@ void safe_print(const std::string& message) {
     std::cout << "Thread with id=" <<  std::this_thread::get_id() << ": " << message << std::endl;
     safePrintMtx.unlock();
 }
+
+void print_vector(std::vector<std::string> vector) {
+    std::cout << "[";
+    for (auto & i : vector)
+        std::cout  << i << ", ";
+    std::cout << "]" << std::endl;
+}
+
--- utils/io/output.h
+++ utils/io/output.h
@@ -1,8 +1,10 @@
 #include <mutex>
 #include <thread>
 #include <iostream>
+#include <vector>
 
 #ifndef INVERTED_INDEX_OUTPUT_H
 #define INVERTED_INDEX_OUTPUT_H
 void safe_print(const std::string& message);
+void print_vector(std::vector<std::string> vector);
 #endif
